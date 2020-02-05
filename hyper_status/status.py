c956103f6faf28259474d94c466ad2f41d9fa621
Xithrius <xithrius@gmail.com>
2020-02-05 00:09:53 -0800

    Finished up v0.1

--- hyper_status/status.py
+++ hyper_status/status.py
@@ -7,59 +7,54 @@
 
 import datetime
 
-import colors as c
+from colors import colors as c
 
 
 class Status:
     '''Outputs date, status and/or a custom string.'''
     
-    def __init__(self, stat: str, warning: str):
-        self.stat = stat
+    def __init__(self, status: str, warning: str):
+        self.status = status
         self.warning = warning
+        self.c = {
+            'fail': c['red'],
+            'ready': c['green'],
+            'date': c['gold'],
+            'warning': c['purple'],
+            'b': c['bold'],
+            'u': c['underline']
+        }
         
-        print(self.insert_items())
+        self.insert_items()
 
-    def insert_items(self):
+    def catch_error(func):
         
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                print(result)
+
+            except IndexError:
+                return print(f'Color could not be found in colors.\nRun the {c["underline"]}colors{c["end"]} module to see options.')
+            
+            except Exception as e:
+                return print(f'Fatal error occured:\n{e}')
+        
+        return wrapper
+
+    def _join(self, lst: list, bind=' ') -> str:
+        return bind.join(str(y) for y in lst)
+
+    @catch_error
+    def insert_items(self, e=c['end'], n=datetime.datetime.now()) -> list:
+        '''
+
+        '''
+        b = self.c['b']
+        n = f"{n.strftime('%b %d, %Y - %A %I:%M:%S')}{n.strftime('%p').lower()}"
         lst = [
-            self.insert_date()
+            f"~> {b}[ {self.c['date']}{n}{e} ]{e} >",
+            f"{b}[ {self.c[self.status]}{self.status.title()}{e} ]{e}:",
+            f"{self.warning.capitalize()}{'.' if self.warning[-1] is not '.' else ''}"
         ]
         return self._join(lst)
-
-    def insert_date(self, bracket_c=c['bold'], date_c=c['blue']):
-        n = datetime.datetime.now()
-        lst = [
-            
-        ]
-
-    def __insert_date(self):
-        n = datetime.datetime.now()
-        s = [
-            f"{c['bold']}[{c['end']} {c.DATE}{n.strftime('%b %d, %Y - %A %I:%M:%S')}{n.strftime('%p').lower()} {c['end']}{c['bold']}]{c['end']}{':' if not warning else ''}",
-            f'{c.BOLD}[{c.END} {warning} {c.BOLD}]{c.END}:' if warning else False,
-            f'{string} '
-        ]
-
-    def warning_string(self, string: str) -> str:
-        '''Creates a string with a custom warning
-        
-        Args:
-            string (str): The string containing the warning.
-
-        Returns:
-            A string with custom colors.
-
-        Raises:
-            ValueError if color and/or warning cannot be found.
-        
-        '''
-        if string in c.values():
-            return c[string]
-        else:
-            raise IndexError(f'Color {string} could not be found in colors.\nRun the {c["underline"]}colors{c["end"]} module to see options.')
-
-    def _join(self, lst: list) -> str:
-        return ' '.join(str(y) for y in lst)
-
-
-Status('warning', 'Cogs have been loaded')
